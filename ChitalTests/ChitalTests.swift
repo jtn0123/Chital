fbfcbb27cedc42860fd6fd9d4c734e475e567609
jtn0123 <jtn0123@gmail.com>
2025-04-11 21:59:04 -0700

    feat: Add initial test structure for Chital with example test case

---
+++ ChitalTests/ChitalTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChitalTests.swift
+//  ChitalTests
+//
+//  Created by Justin Neuhard on 4/11/25.
+//
+
+import Testing
+
+struct ChitalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
